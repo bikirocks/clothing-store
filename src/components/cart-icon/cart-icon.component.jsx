2fc3a4d81a76a8768975d1ac6d452b5bed5d7bcb
bikirocks <bikirocks555@gmail.com>
2023-01-15 18:38:55 +0530

    my new web app

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+
+//import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"; 
+
+import { CartContext } from "../../contexts/cart.context";
+
+import { CartIconContainer, ItemCount, ShoppingIcon } from './cart-icon.styles';
+
+const CartIcon = () => {
+    const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
+  
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  
+    return (
+      <CartIconContainer onClick={toggleIsCartOpen}>
+        <ShoppingIcon className='shopping-icon' />
+        <ItemCount>{cartItemCount}</ItemCount>
+      </CartIconContainer>
+    );
+  };
+  
+  export default CartIcon;
